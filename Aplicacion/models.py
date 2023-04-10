a3dfdf4a2e9796f42d910f70a45cbef7655606bf
Fernanda <fernandaselmen@gmail.com>
2023-04-10 14:12:18 -0300

    views

--- Aplicacion/models.py
+++ Aplicacion/models.py
@@ -6,3 +6,5 @@ class Texto(models.Model):
     titulo = models.CharField(max_length=40)
     subtitulo = models.CharField(max_length=30)
     contenido = models.TextField()
+    def __str__(self):
+        return f' Titulo: {self.titulo} Subtitulo: {self. subtitulo}'
--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from Aplicacion.models import Texto
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 def inicio(request):
     return render(request, 'Aplicacion/index.html')
+
+def publicaciones(request):
+    return render(request, 'Aplicacion/publicaciones.html')
+
+
+
+class TextoListView(ListView):
+    model = Texto
+    template_name = 'Aplicacion/mostrar_view.html'
+   
+class TextoDetailViews(DetailView):
+    model = Texto
+    template_name = 'Aplicacion/texto_detalle_view.html'
+
+class TextoDeleteViews(DeleteView):
+    model = Texto
+    template_name = 'Aplicacion/texto_confirm_view.html'
+    success_url = '/Aplicacion/mostrar_view/'
+
+
+class TextoCreateView(CreateView):
+    model = Texto
+    template_name = 'Aplicacion/texto_form_view.html'
+    success_url = '/Aplicacion/mostrar_view/'
+    fields = ['titulo', 'subtitulo', 'contenido']
+
+class TextoUpdateView(UpdateView):
+    model = Texto
+    template_name = 'Aplicacion/texto_form_view.html'
+    success_url = '/Aplicacion/mostrar_view/'
+    fields = ['titulo', 'subtitulo', 'contenido']
